4134bec3d4822bc9ef70afc9733a312a70da4502
kelvin1095 <k.wong1095@gmail.com>
2023-08-08 20:04:39 +1000

    8Aug

--- pokemon/script.js
+++ pokemon/script.js
@@ -6,24 +6,58 @@ const pikachu = {
     primary: "Electric",
     secondary: "",
   },
+  ability: {
+    primary: "Static",
+    secondary: "",
+    hidden: "Lightning Rod",
+  },
+  baseStats: {
+    hp: 35,
+    attack: 55,
+    defense: 30,
+    spAttack: 50,
+    spDefense: 40,
+    speed: 90,
+  },
   pokedexNo: 25,
-  level: 24,
+  level: 5,
   levelUp: function () {
     this.level = this.level + 1;
   },
 };
 
 const test1 = `
-      <ul>
-          <li>Height: ${pikachu.height} m</li>
-          <li>Weight: ${pikachu.weight} kg</li>
-          <li>Pokedex Number: ${pikachu.pokedexNo}</li>
-      </ul>
-  `;
+<ul>
+  <li>Pokedex Number: ${pikachu.pokedexNo}</li>
+  <li>Height: ${pikachu.height} m</li>
+  <li>Weight: ${pikachu.weight} kg</li>
+</ul>
+<table>
+  <thead>
+    <tr>
+      <th>HP</th>
+      <th>Attack</th>
+      <th>Defense</th>
+      <th>Sp. Atk</th>
+      <th>Sp. Def</th>
+      <th>Speed</th>
+    </tr>
+  </thead>
+  <tr>
+    <td>${pikachu.baseStats.hp}</td>
+    <td>${pikachu.baseStats.attack}</td>
+    <td>${pikachu.baseStats.defense}</td>
+    <td>${pikachu.baseStats.spAttack}</td>
+    <td>${pikachu.baseStats.spDefense}</td>
+    <td>${pikachu.baseStats.speed}</td>
+  </tr>
+</table>
+`;
 
 const newPokemon = document.querySelector("#test1");
 
-const pokemonStats = document.createElement("pikachuStats");
+const pokemonStats = document.createElement("div");
+pokemonStats.setAttribute("id", "PikachuStats");
 pokemonStats.innerHTML = test1;
 newPokemon.append(pokemonStats);
 
@@ -34,6 +68,9 @@ class Pokemon {
     weight,
     typePrimary,
     typeSecondary,
+    primaryAbility,
+    secondaryAbility,
+    hiddenAbility,
     pokedexNo,
     level
   ) {
@@ -44,6 +81,15 @@ class Pokemon {
       primary: typePrimary,
       secondary: typeSecondary,
     };
+    this.ability = {
+      primary: primaryAbility,
+      secondary: secondaryAbility,
+      hidden: hiddenAbility,
+    };
+    this.baseStats = {
+      primary: typePrimary,
+      secondary: typeSecondary,
+    };
     (this.pokedexNo = pokedexNo), (this.level = level);
   }
   levelUp() {
@@ -51,7 +97,7 @@ class Pokemon {
   }
 }
 
-const charmander = new Pokemon("Charmander", 0.6, 8.5, "Fire", "", 4, 5);
+const charmander = new Pokemon("Charmander", 0.6, 8.5, "Fire", "", "blaze", "", "Solar Power", 4, 5);
 
 const test2 = `
       <h2>${charmander.name}</h2>
